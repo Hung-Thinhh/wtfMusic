9ffada4543e443cf2eef74c1537a53cd19d0b5af
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-03-14 21:09:14 +0700

    .

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,57 +1,57 @@
 const BlogPost = require('../models/modles_Test');
-const  {Nuxtify}  = require ("nuxtify-api")
+const { Nuxtify } = require("nuxtify-api")
 const handleHome = async (req, res) => {
-    const getSongmp3 = async() => {
+    const getSongmp3 = async () => {
         console.log("getSongmp3")
         // const url = await Nuxtify.song.getDetail('Z6ZIBDE6');
         // const url = await Nuxtify.getArtist("Jisoo");
-        const url = await Nuxtify.search.getSuggestion('drill');
+        const url = await Nuxtify.getHome();
 
         const timestamp = 1708862634884;
         const date1 = new Date(23960);
 
-const startTime = 20500;
+        const startTime = 20500;
 
-const timestampSeconds = Math.floor(timestamp / 1000); // Chuyển đổi timestamp thành giây
+        const timestampSeconds = Math.floor(timestamp / 1000); // Chuyển đổi timestamp thành giây
 
-const diffSeconds = timestampSeconds - startTime; // Tính hiệu giữa timestamp và startTime
+        const diffSeconds = timestampSeconds - startTime; // Tính hiệu giữa timestamp và startTime
 
         console.log("Difference in seconds:", diffSeconds);
         const date2 = new Date(diffSeconds);
         // const year = date.getFullYear();
-// const month = date.getMonth();
-// const day = date.getDate();
-// const hours = date.getHours();
-// const minutes = date.getMinutes();
-const seconds2 = date2.getSeconds();
-const seconds1 = date1.getSeconds();
+        // const month = date.getMonth();
+        // const day = date.getDate();
+        // const hours = date.getHours();
+        // const minutes = date.getMinutes();
+        const seconds2 = date2.getSeconds();
+        const seconds1 = date1.getSeconds();
 
-// console.log("Year:", year);
-// console.log("Month:", month);
-// console.log("Day:", day);
-// console.log("Hours:", hours);
-// console.log("Minutes:", minutes);
-console.log("Seconds1:", seconds1);
-console.log("Seconds2:", seconds2);
+        // console.log("Year:", year);
+        // console.log("Month:", month);
+        // console.log("Day:", day);
+        // console.log("Hours:", hours);
+        // console.log("Minutes:", minutes);
+        console.log("Seconds1:", seconds1);
+        console.log("Seconds2:", seconds2);
 
-console.log(date2);
+        console.log(date2);
         return res.json(url);
     }
     getSongmp3()
 
     //test database
-//     BlogPost.find()
-//   .then(BlogPost => {
-//       return res.json(BlogPost)
-//   })
-//   .catch(error => {
-//     return res.status(400).json({ error: error });
-//   });
- 
+    //     BlogPost.find()
+    //   .then(BlogPost => {
+    //       return res.json(BlogPost)
+    //   })
+    //   .catch(error => {
+    //     return res.status(400).json({ error: error });
+    //   });
+
     // const user = await userService.getSongmp3()
     // return res.send(user)
 }
 module.exports = {
     handleHome,
-    
+
 }   
