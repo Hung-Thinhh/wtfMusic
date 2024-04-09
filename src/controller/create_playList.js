aeb989dcf6fd6a245852c56b5000a07cc58aecea
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-04-09 14:21:43 +0700

    modified:   src/controller/create_playList.js

--- src/controller/create_playList.js
+++ src/controller/create_playList.js
@@ -1,21 +1,60 @@
 const playList = require('../models/playlist_model');
+import User from "../models/user_model.js";
 const { v4: uuidv4 } = require('uuid');
 const addToPlayList = async (req, res) => {
     try {
-        const id = uuidv4().substring(0, 8).toUpperCase();
-
         const playListData = req.body.data;
-        const final = {
-            id,
-            ...playListData
-        }
-        console.log("playListỬ",final)
-        // const newplayList = new playList(playListData);
-        // const savedplayList = await newplayList.save();
 
-        res.json(playListData);
-    } catch(err) {
-        console.log(err);
+        if (playListData) {
+            const user = await User.findOne({
+                $or: [{ username: playListData.artistsId }],
+            });
+            
+            if (!user) {
+                return res.status(404).send({error: 'User not found'});
+            }
+
+            let playlist = await playList.findOne({ artistsId: user.id });
+
+            // Nếu playlist đã tồn tại, giữ nguyên playlistId
+            if (playlist) {
+                const updatedPlaylist = await playList.findOneAndUpdate({ artistsId: user.id }, {
+                    $addToSet: { songid: playListData.songid[0] },
+                    $set: {
+                        playlistname: playListData.artistsId,
+                        genresid: [],
+                        thumbnail: playListData.thumbnail,
+                        description: playListData.description,
+                        like: playListData.like,
+                        listen: playListData.listen,
+                    },
+                }, { new: true });
+
+                console.log(updatedPlaylist);
+                return res.json(updatedPlaylist);
+            }
+        
+            // Nếu playlist chưa tồn tại, tạo mới playlist với playlistId
+            const id = uuidv4().substring(0, 8).toUpperCase();
+            const newPlaylist = new playList({
+                playlistId: id,
+                artistsId: user.id,
+                playlistname: playListData.artistsId,
+                songid: playListData.songid,
+                genresid: [],
+                thumbnail: playListData.thumbnail,
+                description: playListData.description,
+                like: playListData.like,
+                listen: playListData.listen,
+            });
+
+            const createdPlaylist = await newPlaylist.save();
+            console.log(createdPlaylist);
+            return res.json(createdPlaylist);
+        }
+
+    } catch (err) {
+        return res.status(500).send({error: err.message});
     }
 }
 
