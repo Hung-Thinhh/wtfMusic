891eab09f384e40f5dba38790f5e2671639ef8d8
Hung-Thinhh <hungthinhh2003@gmail.com>
2024-04-13 00:11:16 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/controller/User.js 	modified:   src/services/User_service.js 	new file:   uploads/3d0a8f32871851460809.jpg 	new file:   uploads/8e567d8906a3d0fd89b2.jpg 	new file:   uploads/banh1.png 	new file:   uploads/banh3.png

--- src/services/User_service.js
+++ src/services/User_service.js
@@ -1,62 +1,80 @@
 import User from "../models/user_model.js";
-import {checkPassword,hashPassword} from "./Authentication_service.js"
+import { checkPassword, hashPassword } from "./Authentication_service.js";
 
 const getInfor = async (id) => {
-    let user = await User.findOne({ id: id });
-    if (user) {
-        return {
-            EM: "ok!",
-            EC: "0",
-            DT: user
-          };
-    } else {
-      return false;
-    }
-
-}
-const updateInfor = async (data,id) => {
-  const newInfor = data.infor
-  const updateUser = await User.findOneAndUpdate({ id: id }, newInfor, {
-    upsert: true
-  }).select('-_id username email birthday');
-  if (updateUser) {
-      return {
-          EM: "ok!",
-          EC: "0",
-          DT: updateUser
-        };
+  let user = await User.findOne({ id: id });
+  if (user) {
+    return {
+      EM: "ok!",
+      EC: "0",
+      DT: user,
+    };
   } else {
     return false;
   }
-
-}
-const changepassword = async (data,id) => {
-  const newInfor = data
-  let updateUser
+};
+const updateInfor = async (data, id) => {
+  const newInfor = data.infor;
+  const updateUser = await User.findOneAndUpdate({ id: id }, newInfor, {
+    upsert: true,
+    new: true,
+  }).select("-_id username email birthday avt");
+  if (updateUser) {
+    return {
+      EM: "updated successfully",
+      EC: "0",
+      DT: updateUser,
+    };
+  } else {
+    console.log(
+      "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"
+    );
+    return false;
+  }
+};
+const changepassword = async (data, id) => {
+  const newInfor = data;
+  let updateUser;
   const user = await User.findOne({
-     id: id 
+    id: id,
   });
   if (user && user.password) {
-    let isCorrectPassword = await checkPassword(newInfor.password, user.password);
+    let isCorrectPassword = await checkPassword(
+      newInfor.password,
+      user.password
+    );
     if (isCorrectPassword) {
       let hashPass = hashPassword(newInfor.newPassword);
-       updateUser = await User.findOneAndUpdate({ id: id }, {password:hashPass}, { upsert: true });
+      updateUser = await User.findOneAndUpdate(
+        { id: id },
+        { password: hashPass },
+        { upsert: true }
+      );
+    } else {
+      return {
+        EM: "Bạn đã sai mật khẩu!",
+        EC: "1",
+        DT: "",
+      };
     }
   }
   if (updateUser) {
-      return {
-          EM: "ok!",
-          EC: "0",
-          DT: ''
-        };
+    return {
+      EM: "ok!",
+      EC: "0",
+      DT: "",
+    };
   } else {
-    return false;
+    return {
+      EM: "error from server",
+      EC: "-1",
+      DT: "",
+    };
   }
-
-}
+};
 
 module.exports = {
   getInfor,
   updateInfor,
-  changepassword
-}
+  changepassword,
+};
