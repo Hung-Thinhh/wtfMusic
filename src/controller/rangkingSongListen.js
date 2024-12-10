6665abd1d8699d64768033e8962489cc9e945308
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-12-10 20:04:47 +0700

    admin chart api

--- src/controller/rangkingSongListen.js
+++ src/controller/rangkingSongListen.js
@@ -1,17 +1,26 @@
-import { getSongRankListen } from "../services/getdateSongRankListen_service.js"
+import { getSongRankListen } from "../services/getdateSongRankListen_service.js";
+
 const getSongRankListenControl = async (req, res) => {
   const songId = req.params.id;
-  const data = await getSongRankListen(songId)
+  const range = req.query.range;
+  const start = req.query.start;
+  const data = await getSongRankListen(songId, range, start);
+  console.log("song ls", data);
   if (data.EC == "0") {
     return res.status(200).json({
       EM: data.EM,
       EC: "0",
-      DT: { data: data.DT },
+      DT:  data.DT,
+    });
+  } else {
+    return res.status(400).json({
+      EM: data.EM,
+      EC: data.EC,
     });
   }
-}
+};
 
 module.exports = {
-  getSongRankListenControl
-}
+  getSongRankListenControl,
+};
 
