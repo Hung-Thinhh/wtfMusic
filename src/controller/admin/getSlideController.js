927a3d24ac56d5a5c7fd81fd44de6978136cad4f
Hung-Thinhh <hungthinhh2003@gmail.com>
2024-12-25 00:13:52 +0700

    s

--- src/controller/admin/getSlideController.js
+++ src/controller/admin/getSlideController.js
@@ -98,6 +98,7 @@ const insertSildeController = async (req, res) => {
                     } catch (error) {
                         return res.status(400).json({ error: error.message });
                     }
+                    
                 });
 
                 uploadStream.end(file.buffer);
