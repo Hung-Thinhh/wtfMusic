a0fdfaed07faa0281cdac297ffbe630bddb6bc1f
Hung-Thinhh <hungthinhh2003@gmail.com>
2025-06-29 22:44:02 +0700

    home

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import {handleHome} from '../controller/homeController'
+require('dotenv').config()
 
 const router = express.Router();
 
 const initWebRouter = (app) => {
-    router.get("/", handleHome);
+    router.get("/", (req, res) => {
+        res.send("Hello World from web router",process.env.CLIENT);
+    });
     // router.get("/user", homeController.handleCreateUser);
     // router.post("/user/create-user", homeController.handleCreateNewUser);
     // router.post("/delete-user/:id", homeController.handleDeleteUser);
