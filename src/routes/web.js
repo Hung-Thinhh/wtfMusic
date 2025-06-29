df6d5176125b89820c2a3840f5675218bb68e02b
Hung-Thinhh <hungthinhh2003@gmail.com>
2025-06-29 23:01:53 +0700

    home json

--- src/routes/web.js
+++ src/routes/web.js
@@ -5,14 +5,13 @@ require('dotenv').config()
 const router = express.Router();
 
 const initWebRouter = (app) => {
-    router.get("/", (req, res) => {
-        res.send("Hello World from web router",process.env.CLIENT);
+   router.get("/", (req, res) => {
+    res.json({
+        message: "Hello World from web router",
+        client: process.env.CLIENT
     });
-    // router.get("/user", homeController.handleCreateUser);
-    // router.post("/user/create-user", homeController.handleCreateNewUser);
-    // router.post("/delete-user/:id", homeController.handleDeleteUser);
-    // router.post("/edit-user/", homeController.handleEditUser);
-    // router.get("/edit-user/:id", homeController.handleEdit);
+});
+
     return app.use("/", router);
 }
 
